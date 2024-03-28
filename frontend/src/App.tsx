211eadc156105949390cac827b288e7045686a68
darienvr <118293353+darienvr@users.noreply.github.com>
2024-03-28 00:29:18 -0500

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-400 text-cyan-950'>
+        Hola Mundo
+      </div>
+    </>
+  )
+}
+
+export default App
