ed72618a68ad390dd217e34dae9f1b0d505e385c
darienvr <118293353+darienvr@users.noreply.github.com>
2024-03-29 12:24:38 -0500

    Add the categories and cart button

--- frontend/src/views/ProductsView.tsx
+++ frontend/src/views/ProductsView.tsx
@@ -4,24 +4,25 @@ import { useNavigate } from 'react-router-dom';
 
 function ProductsView() {
 
-    const [categories, setCategories] = useState<any[]>([])
-    const [products, setProducts] = useState<any[]>([])
+    const [categories, setCategories] = useState<any[]>([]);
+    const [products, setProducts] =  useState<any[]>([]);
+    const [selectCategory, setSelectCategory] = useState<string>('All');
+    const [filterProducts, setFilterProducts] = useState<any[]>([]);
 
     const navigate = useNavigate(); 
 
     const getProducts = (url: string) => {
         axiosClient.get(url)
             .then((response:any)=>{
-                console.log(response.data.data)
                 setProducts(response.data.data)
+                setFilterProducts(response.data.data)
             });
     };
 
     const getCategories = (url: string) => {
         axiosClient.get(url)
             .then((response:any)=>{ 
-                console.log(response.data.categories);
-                setCategories(response.data.categories);
+                setCategories([{ id: 'All', category: 'All' }, ...response.data.categories]);
             });
     };
 
@@ -30,8 +31,25 @@ function ProductsView() {
         getProducts('/product')
     },[])
 
-    const onClick = (id:any) => {
+    const onClick = (id:string) => {
         navigate(`/product/${id}`)
+    }
+
+    const handleCategory = (category_id: string) => {
+        setSelectCategory(category_id)
+    }
+
+    useEffect(() => {
+        if (selectCategory === "All") {
+          setFilterProducts(products);
+        } else {
+          setFilterProducts(products.filter(item => item.category_id == selectCategory));
+        }
+      }, [selectCategory, products]);
+
+
+    const addCart = () => {
+        alert('Agregado al carrito')
     }
 
   return (
@@ -39,16 +57,19 @@ function ProductsView() {
         <h1 className='font-semibold text-4xl text-center py-5'>ECOMMERCE - LARAVEL | REACT</h1>
         <div className='flex gap-5 justify-center pt-3 pb-10'>
           {categories.map(category => (
-            <button className='rounded-md border-2 px-4 border-gray-300 bg-gray-100 hover:bg-gray-200' key={category.id}>{category.category}</button>
+            <button onClick={()=>handleCategory(category.id)} className='rounded-md border-2 px-4 border-gray-300 bg-gray-100 hover:bg-gray-200' key={category.id}>{category.category}</button>
           ))}
         </div>
         <div className='grid grid-cols-3 w-4/5 mx-auto gap-8 my-3'>
-            {products.map(product=> (
-                <div onClick={()=>onClick(product.id)} key={product.id} className='border-2 shadow-lg rounded-md min-h-[400px] cursor-pointer'>
-                    <img className='h-[90%]' src={product.image} alt={product.name} />
-                    <div className='flex justify-between px-2 items-center text-lg h-[10%]'>
-                        <h3 className='font-bold'>{product.name}</h3>
-                        <p className='text-gray-500 font-semibold'>{product.price}</p>
+            {filterProducts.map(product=> (
+                <div key={product.id} className='border-2 shadow-lg rounded-md min-h-[400px] '>
+                    <img onClick={()=>onClick(product.id)} className='h-[85%] w-[100%] cursor-pointer' src={product.image} alt={product.name} />
+                    <div className='px-2 items-center text-lg h-[15%]'>
+                        <div className='flex justify-between'>
+                            <h3 className='font-bold'>{product.name}</h3>
+                            <p className='text-gray-500 font-semibold'>S./{product.price}</p>
+                        </div>
+                        <button onClick={()=>addCart()} className='bg-orange-400 px-3 rounded-md text-white font-semibold'>Add Cart</button>
                     </div>
                 </div>
             ))}
