d6ad73b549c7842532c441d52f58a6ca2aa10b7d
darienvr <118293353+darienvr@users.noreply.github.com>
2024-03-30 22:44:05 -0500

    add cart component

---
+++ frontend/src/views/CartView.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { useStateContext } from '../contexts/ContextProvider';
+import { ContextType } from '../types';
+import axiosClient from '../axios';
+
+const CartView = () => {
+    const { cart, setCart } = useStateContext() as ContextType;
+    const [productDetails, setProductDetails] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchProductDetails = async () => {
+            const promises = cart.map((item) =>
+                axiosClient.get(`/product/${item.product_id}`).then((response: any) => {
+                    console.log(response.data.data);
+                    return response.data.data;
+                })
+            );
+
+            const details = await Promise.all(promises);
+            setProductDetails(details);
+        };
+
+        fetchProductDetails();
+    }, [cart]);
+
+    const totalPrice = productDetails.reduce((total, product) => {
+        const cartItem = cart.find((item) => item.product_id === product.id);
+        return total + (product.price * (cartItem ? cartItem.amount : 0));
+    }, 0);
+
+    const handleDelete = (id: string) => {
+        setCart(cart.filter(item => item.product_id !== id));
+    };
+
+    return (
+        <div className='mt-20'>
+            {productDetails.map((product) => {
+                const cartItem = cart.find((item) => item.product_id === product.id);
+                return (
+                    <div key={product.id} className='flex w-[50%] mx-auto justify-between items-center mb-5'>
+                        <div className='flex gap-3 items-center'>
+                            <img className='w-[100px] h-20' src={product.image} alt="" />
+                            <div>
+                                <h1>{product.name}</h1>
+                                <p>{product.price}</p>
+                            </div>
+                        </div>
+                        <p>Amount: {cartItem ? cartItem.amount : 0}</p>
+                        <p>Precio: {product.price * (cartItem ? cartItem.amount : 0)}</p>
+                        <button onClick={() => handleDelete(product.id)} className='text-red-500'>Delete</button>
+                    </div>
+                );
+            })}
+            <div className='mx-auto w-fit'>
+                <h1>Total Price: {totalPrice}</h1>
+            </div>
+        </div>
+    );
+};
+
+export default CartView;
--- frontend/src/views/ProductsView.tsx
+++ frontend/src/views/ProductsView.tsx
@@ -1,60 +1,23 @@
-import axiosClient from '../axios'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useStateContext } from '../contexts/ContextProvider'
+import { ContextType } from '../types';
+import { Link } from 'react-router-dom';
 
 function ProductsView() {
 
-    const [categories, setCategories] = useState<any[]>([]);
-    const [products, setProducts] =  useState<any[]>([]);
-    const [selectCategory, setSelectCategory] = useState<string>('All');
-    const [filterProducts, setFilterProducts] = useState<any[]>([]);
+    const { categories, filterProducts, handleCategory, addCart, cart } = useStateContext() as ContextType;
 
     const navigate = useNavigate(); 
-
-    const getProducts = (url: string) => {
-        axiosClient.get(url)
-            .then((response:any)=>{
-                setProducts(response.data.data)
-                setFilterProducts(response.data.data)
-            });
-    };
-
-    const getCategories = (url: string) => {
-        axiosClient.get(url)
-            .then((response:any)=>{ 
-                setCategories([{ id: 'All', category: 'All' }, ...response.data.categories]);
-            });
-    };
-
-    useEffect(()=>{
-        getCategories('/category');
-        getProducts('/product')
-    },[])
 
     const onClick = (id:string) => {
         navigate(`/product/${id}`)
     }
 
-    const handleCategory = (category_id: string) => {
-        setSelectCategory(category_id)
-    }
-
-    useEffect(() => {
-        if (selectCategory === "All") {
-          setFilterProducts(products);
-        } else {
-          setFilterProducts(products.filter(item => item.category_id == selectCategory));
-        }
-      }, [selectCategory, products]);
-
-
-    const addCart = () => {
-        alert('Agregado al carrito')
-    }
-
   return (
     <>
         <h1 className='font-semibold text-4xl text-center py-5'>ECOMMERCE - LARAVEL | REACT</h1>
+        <Link to={'/cart'}>Cart</Link>
         <div className='flex gap-5 justify-center pt-3 pb-10'>
           {categories.map(category => (
             <button onClick={()=>handleCategory(category.id)} className='rounded-md border-2 px-4 border-gray-300 bg-gray-100 hover:bg-gray-200' key={category.id}>{category.category}</button>
@@ -69,10 +32,16 @@ function ProductsView() {
                             <h3 className='font-bold'>{product.name}</h3>
                             <p className='text-gray-500 font-semibold'>S./{product.price}</p>
                         </div>
-                        <button onClick={()=>addCart()} className='bg-orange-400 px-3 rounded-md text-white font-semibold'>Add Cart</button>
+                        <button onClick={()=>addCart(product.id)} className='bg-orange-400 px-3 rounded-md text-white font-semibold'>Add Cart</button>
                     </div>
                 </div>
             ))}
+        </div>
+
+        <div>
+            <pre>
+                {JSON.stringify(cart, null, 2)}
+            </pre>
         </div>
     </>
   )
