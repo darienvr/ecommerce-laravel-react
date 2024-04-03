442c17a743d6ff010261a4e1be8362c8a75549d4
darienvr <118293353+darienvr@users.noreply.github.com>
2024-04-03 11:28:05 -0500

    fix total amount in navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,6 +7,8 @@ const Navbar = () => {
 
     const { cart } = useStateContext() as ContextType;
 
+    const totalAmount = cart.reduce((acc, item)=>acc + item.amount, 0);
+
   return (
     <nav className='bg-zinc-800 h-20 text-white flex items-center'>
             <div className='flex justify-between w-[90%] mx-auto text-xl'>
@@ -14,7 +16,7 @@ const Navbar = () => {
                     <p>Logo</p>
                 </div>
                 <div className='flex gap-10'>
-                    <Link to={'/cart'}>Cart ({cart.length})</Link>
+                    <Link to={'/cart'}>Cart ({totalAmount})</Link>
                     <p>Login</p>
                 </div>
             </div>
