8889e88ecd2fed5719a215b0d0b54157d662a17e
darienvr <118293353+darienvr@users.noreply.github.com>
2024-04-07 22:51:20 -0500

    add order sumary in cart view

--- frontend/src/views/CartView.tsx
+++ frontend/src/views/CartView.tsx
@@ -11,6 +11,8 @@ const CartView = () => {
         setCart(cart.filter(item => item.product_id !== id));
     };
 
+    const totalAmount = cart.reduce((acc, item)=>acc + item.amount, 0);
+
     const handleIncrease = (id:Product['id']) => {
         setCart((prev:[]) => {
             const cartItem = cart.find((item) => item.product_id === id);
@@ -53,46 +55,68 @@ const CartView = () => {
     return (
         <>
             <Navbar />
-            <div className='mt-10 w-[43%] mx-auto'>
+            <div className='mt-10 w-[70%] mx-auto'>
                 {loading ? (
                     <div className='text-5xl text-center font-semibold'>LOADING...</div>
                 ) : (
-                    <>
-                        {productDetails.map((product:any) => {
-                        const cartItem = cart.find((item) => item.product_id === product.id);
-                        return (
-                            <div key={product.id}>
-                                <div  className='flex justify-between items-center my-5 text-lg'>
-                                    <div className='flex gap-3 items-center'>
-                                        <img className='w-[130px] rounded-sm h-28' src={product.image} alt="" />
+                    <div className='flex'>
+                        <div className='w-[60%] px-10'>
+                            {productDetails.map((product:any) => {
+                            const cartItem = cart.find((item) => item.product_id === product.id);
+                            return (
+                                <div key={product.id}>
+                                    <div  className='flex justify-between items-center my-5 text-lg'>
+                                        <div className='flex gap-3 items-center'>
+                                            <img className='w-[120px] rounded-md h-28' src={product.image} alt="" />
+                                            <div>
+                                                <h1 className='font-semibold'>{product.name}</h1>
+                                                
+                                                <div className='flex items-center'>
+                                                    <p className='text-gray-500 text-sm'>Quantity:</p>
+                                                    <button onClick={()=>handleDecrease(product.id)} className='border px-2 py-0 border-black rounded-sm'>-</button>
+                                                    <p className='mx-2'>{cartItem ? cartItem.amount : 0}</p>
+                                                    <button onClick={()=>handleIncrease(product.id)} className='border px-2 py-0 border-black rounded-sm'>+</button>
+                                                </div> 
+                                            </div>
+                                        </div>
                                         <div>
-                                            <h1 className='font-semibold'>{product.name}</h1>
-                                            
-                                            <div className='flex items-center'>
-                                                <p className='text-gray-500 text-sm'>Quantity:</p>
-                                                <button onClick={()=>handleDecrease(product.id)} className='border px-2 py-0 border-black rounded-sm'>-</button>
-                                                <p className='mx-2'>{cartItem ? cartItem.amount : 0}</p>
-                                                <button onClick={()=>handleIncrease(product.id)} className='border px-2 py-0 border-black rounded-sm'>+</button>
-                                            </div> 
+                                            <p className='font-bold'>S./ {product.price}</p>
+                                            <button onClick={() => handleDelete(product.id)} className='text-red-500'>Delete</button>
                                         </div>
                                     </div>
-                                    <div>
-                                        <p className='font-bold'>S./ {product.price}</p>
-                                        <button onClick={() => handleDelete(product.id)} className='text-red-500'>Delete</button>
-                                    </div>
+                                    <hr />
                                 </div>
-                                <hr />
+                            );
+                            })}
+                            <div className='flex justify-between my-5 items-center'>
+                                <Link className='border-gray-900 border-2 rounded-md px-3 py-1' to='/'>Continue Shopping</Link>
                             </div>
-                        );
-                    })}
-                    <div className='flex justify-between my-5 items-center'>
-                        <Link className='border-gray-900 border-2 rounded-md px-3 py-1' to='/'>Continue Shopping</Link>
-                        <h1 className='text-xl font-semibold'>SubTotal: S./{totalPrice}</h1>
+                            
+                        </div>
+                        <div className='w-[40%]'>
+                            <div className='border h-fit w-[75%] mx-auto my-5 flex flex-col justify-between p-3 rounded-md border-black'>
+                                <h1 className='font-bold pb-5'>ORDER SUMARY</h1>
+                                <span className='flex justify-between'>
+                                    <h3>Subtotal</h3><p>${totalPrice}</p>
+                                </span>
+                                <hr/>
+                                <span className='flex justify-between'>
+                                    <h3>Shipping</h3><p>$5</p>
+                                </span>
+                                <hr/>
+                                <span className='flex justify-between'>
+                                    <h3>Tax</h3><p>${totalAmount*18}</p>
+                                </span>
+                                <hr/>
+                                <span className='flex justify-between pt-5'>
+                                    <h2 className='font-semibold'>Order Total</h2><p>${totalPrice}</p>
+                                </span>
+                            </div>
+                            <div className='flex justify-center mb-10'>
+                                <Link to={'/payment'} className='bg-blue-400 px-5 py-2 rounded-md'>Proceed To Checkout</Link>
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex justify-center mb-10'>
-                        <Link to={'/payment'} className='bg-blue-400 px-5 py-2 rounded-md'>Proceed To Checkout</Link>
-                    </div>
-                    </>
                 )}
             </div>
         </>
