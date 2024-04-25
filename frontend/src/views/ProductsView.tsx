fe7460aec6f76b82f39dac4f4e2c3095da51af00
darienvr <118293353+darienvr@users.noreply.github.com>
2024-04-24 21:58:27 -0500

    add onchange in search

--- frontend/src/views/ProductsView.tsx
+++ frontend/src/views/ProductsView.tsx
@@ -2,14 +2,16 @@ import { useStateContext } from '../contexts/ContextProvider'
 import { ContextType } from '../types';
 import Navbar from '../components/Navbar';
 import ProductsList from '../components/ProductsList';
+import { useState } from 'react';
 
 function ProductsView() {
 
     const { categories, handleCategory, selectCategory } = useStateContext() as ContextType;
+    const [input, setInput] = useState('')
 
     const onSubmit = (e:any) => {
         e.preventDefault()
-        alert('Click in Input Button')
+        alert(input)
     }
 
   return (
@@ -21,7 +23,7 @@ function ProductsView() {
                     <div className='p-5'>
                         <h1 className='text-xl font-semibold pb-3'>Search Product</h1>
                         <form  onSubmit={(e)=>onSubmit(e)} className='flex items-center'>
-                            <input className='pl-1 shadow-md bg-gray-200 rounded-l-md w-[85%] h-[35px] border' type="text" />
+                            <input value={input} onChange={(e)=>setInput(e.target.value)} className='pl-1 shadow-md bg-gray-200 rounded-l-md w-[85%] h-[35px] border' type="text" />
                             <button type='submit' className='shadow-md mx-auto w-[15%] h-[35px] flex justify-center items-center rounded-r-md border  hover:bg-slate-200 transition-colors'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
