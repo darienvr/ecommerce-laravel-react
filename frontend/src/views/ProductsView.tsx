17d088097cded84cc471b1e623df1a116459df86
darienvr <118293353+darienvr@users.noreply.github.com>
2024-04-28 22:25:12 -0500

    add validations and types in input

--- frontend/src/views/ProductsView.tsx
+++ frontend/src/views/ProductsView.tsx
@@ -9,9 +9,14 @@ function ProductsView() {
     const { categories, handleCategory, selectCategory, searchSubmit } = useStateContext() as ContextType;
     const [input, setInput] = useState('')
 
-    const onSubmit = (e:any) => {
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        searchSubmit(input)
+        searchSubmit(input); 
+    }
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/\s{1,}/g, ' '); // Reemplaza más de 2 espacios seguidos por 2 espacios
+        setInput(value);
     }
 
   return (
@@ -23,7 +28,7 @@ function ProductsView() {
                     <div className='p-5'>
                         <h1 className='text-xl font-semibold pb-3'>Search Product</h1>
                         <form  onSubmit={(e)=>onSubmit(e)} className='flex items-center'>
-                            <input value={input} onChange={(e)=>setInput(e.target.value)} className='pl-1 shadow-md bg-gray-200 rounded-l-md w-[85%] h-[35px] border' type="text" />
+                            <input value={input.trimStart()} onChange={handleChange}  className='pl-1 shadow-md bg-gray-200 rounded-l-md w-[85%] h-[35px] border' type="text" />
                             <button type='submit' className='shadow-md mx-auto w-[15%] h-[35px] flex justify-center items-center rounded-r-md border  hover:bg-slate-200 transition-colors'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
