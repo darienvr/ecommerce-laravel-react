b0ca6c5697987461d6fd5b4c30d14ec806f30b44
darienvr <118293353+darienvr@users.noreply.github.com>
2024-05-13 23:06:37 -0500

    add style in guest layout

--- frontend/src/components/GuestLayout.tsx
+++ frontend/src/components/GuestLayout.tsx
@@ -10,12 +10,12 @@ const GuestLayout = () => {
 
 
   return (
-    <div id='guestLayout'>
-        <div className='contenedor-form'>
-            <div className='contenedor-img'>
-                <img className='img-login' src={loginImg}></img>
+    <div className='h-[100vh] flex items-center justify-center'>
+        <div className='w-[1000px] flex h-[60%] items-center bg-blue-300'>
+            <div className='w-[50%]'>
+                <img className='rounded' src={loginImg}></img>
             </div>
-            <div className='contenedor-formulario animated fadeInDown'>
+            <div className='p-5'>
                 <Outlet />
             </div>
         </div>
