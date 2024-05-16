6e80eebecefaf1e90c989644d8159b2d1fa8bc93
darienvr <118293353+darienvr@users.noreply.github.com>
2024-05-15 22:34:09 -0500

    improved user interface in signup

--- frontend/src/views/Signup.tsx
+++ frontend/src/views/Signup.tsx
@@ -34,16 +34,16 @@ const Signup = () => {
   
     return (
       <div>
-          <h3 className='title-signup'>Ingrese sus datos </h3>
+          <h3 className='text-xl'>Ingrese sus datos </h3>
           <form onSubmit={onSubmit} className='formulario'>
-            <label className='form-label'><b>Nombre</b> </label>
-            <input ref={nameRef}  className='form-control' type='text'></input> 
-            <label className='form-label'><b>Email</b> </label> 
-            <input ref={emailRef} className='form-control'  type='email'></input>
-            <label className='form-label'><b>Contraseña</b> </label>
-            <input ref={passwordRef} className='form-control'  type='password'></input>
-            <label className='form-label'><b>Confirmar contraseña</b></label> 
-            <input ref={passwordConfirmationRef} className='form-control'  type='password'></input>
+            <label className='form-label'><b>Nombre</b> </label><br/>
+            <input ref={nameRef}  className='rounded w-[80%]' type='text'></input> <br/>
+            <label className='form-label'><b>Email</b> </label> <br/>
+            <input ref={emailRef} className='rounded w-[80%]'  type='email'></input><br/>
+            <label className='form-label'><b>Contraseña</b> </label><br/>
+            <input ref={passwordRef} className='rounded w-[80%]'  type='password'></input><br/>
+            <label className='form-label'><b>Confirmar contraseña</b></label> <br/>
+            <input ref={passwordConfirmationRef} className='rounded w-[80%]'  type='password'></input><br/>
             <button className='btn-login' type='submit'>Registrar</button>
             <div className='contenedor-registrarse'>
               <p>¿Ya tiene una cuenta?</p>
