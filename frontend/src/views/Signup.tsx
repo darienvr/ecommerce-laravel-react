5ffe8f589d22163b0bfa66cd3a97c6547a3a385b
darienvr <118293353+darienvr@users.noreply.github.com>
2024-05-18 21:28:07 -0500

    improved ui in signup

--- frontend/src/views/Signup.tsx
+++ frontend/src/views/Signup.tsx
@@ -34,20 +34,20 @@ const Signup = () => {
   
     return (
       <div>
-          <h3 className='text-xl'>Ingrese sus datos </h3>
+          <h3 className='text-xl pb-5'>Ingrese sus datos </h3>
           <form onSubmit={onSubmit} className='formulario'>
             <label className='form-label'><b>Nombre</b> </label><br/>
-            <input ref={nameRef}  className='rounded w-[80%]' type='text'></input> <br/>
+            <input ref={nameRef}  className='rounded border w-[80%] border-gray-400 pl-2' type='text'></input> <br/>
             <label className='form-label'><b>Email</b> </label> <br/>
-            <input ref={emailRef} className='rounded w-[80%]'  type='email'></input><br/>
+            <input ref={emailRef} className='rounded border w-[80%] border-gray-400 pl-2'  type='email'></input><br/>
             <label className='form-label'><b>Contraseña</b> </label><br/>
-            <input ref={passwordRef} className='rounded w-[80%]'  type='password'></input><br/>
+            <input ref={passwordRef} className='rounded border w-[80%] border-gray-400 pl-2'  type='password'></input><br/>
             <label className='form-label'><b>Confirmar contraseña</b></label> <br/>
-            <input ref={passwordConfirmationRef} className='rounded w-[80%]'  type='password'></input><br/>
-            <button className='btn-login' type='submit'>Registrar</button>
-            <div className='contenedor-registrarse'>
+            <input ref={passwordConfirmationRef} className='rounded border w-[80%] border-gray-400 pl-2'  type='password'></input><br/>
+            <button className='rounded-md bg-blue-800 text-white py-1 px-4 my-3' type='submit'>Registrar</button>
+            <div className='flex'>
               <p>¿Ya tiene una cuenta?</p>
-              <a className='btn-link' href='/login'>Iniciar Sesion</a>
+              <a className='text-blue-900 pl-2 hover:underline' href='/login'>Iniciar Sesion</a>
             </div>
             
           </form>
