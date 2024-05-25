23b53d164789dd8e1a4682fc9572f8f3a8ec7449
darienvr <118293353+darienvr@users.noreply.github.com>
2024-05-24 22:15:26 -0500

    add route api register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+    Route::post('/logout',[AuthController::class, 'logout']);
+    Route::apiResource('/users', UserController::class);
 });
 
 Route::apiResource('category', CategoryController::class);
